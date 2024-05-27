183a2f1e5f78d11879426cc0b07c9cfc8c03d7d8
Zaidan Ahmad <58198569+FactSwift@users.noreply.github.com>
2024-05-28 00:42:37 +0700

    Add files via upload

---
+++ UAS-Basdat/fungsimenu/functions.php
@@ -0,0 +1,25 @@
+<?php
+function showProfile() {
+    
+    echo "<h2>Profil Anda</h2>";
+    
+}
+
+function showTransactions() {
+    
+    echo "<h2>Riwayat Transaksi</h2>";
+    
+}
+
+function showReports() {
+    
+    echo "<h2>Laporan</h2>";
+    
+}
+
+function showSettings() {
+    
+    echo "<h2>Pengaturan</h2>";
+    
+}
+?>
